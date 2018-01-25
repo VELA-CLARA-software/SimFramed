043a689316b5b25451a1a0519fd56896e483c7f6
James Jones <james.jones@stfc.ac.uk>
2018-01-25 11:28:05 +0000

    Small Changes
    Former-commit-id: 3a8ef788a52a01924e8fa4ad1d64cbfaa44e9402

--- ASTRAFramework/Simple/constraintsTest.py
+++
@@ -1,9 +0,0 @@
-from constraints import *
-
-cons = constraintsClass()
-
-constraintsList = {
-'1':{'type': 'lessThan', 'value': 23, 'limit': 0, 'weight': 1},
-'2': {'type': 'greaterThan', 'value':0,'limit':12,'weight':1}}
-
-print cons.constraints(constraintsList)
