09ebde83b205df8913907d7ca2e1f984473f5ab9
James Jones <james.jones@stfc.ac.uk>
2018-03-14 22:56:08 +0000

    Split Framework code-wise
    Former-commit-id: 7e80e14da3784c08b7014867efa67917cc3b802f

--- ASTRAFramework/ASTRAGeneral/optimise_GA_longitudinal.py
+++ ASTRAFramework/ASTRAGeneral/optimise_GA_longitudinal.py
@@ -17,6 +17,60 @@ import csv
 import shutil
 import uuid
 
+def eaSimple(population, toolbox, cxpb, mutpb, ngen, stats=None,
+             halloffame=None, verbose=__debug__):
+
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in population if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    if halloffame is not None:
+        halloffame.update(population)
+
+    record = stats.compile(population) if stats else {}
+    logbook.record(gen=0, nevals=len(invalid_ind), **record)
+    if verbose:
+        print logbook.stream
+
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
+        # Select the next generation individuals
+        offspring = toolbox.select(population, len(population))
+
+        # Vary the pool of individuals
+        offspring = algorithms.varAnd(offspring, toolbox, cxpb, mutpb)
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Update the hall of fame with the generated individuals
+        if halloffame is not None:
+            halloffame.update(offspring)
+            with open('longitudinal_best_solutions_running.csv','wb') as out:
+                csv_out=csv.writer(out)
+                for row in halloffame:
+                    csv_out.writerow(row)
+
+        # Replace the current population by the offspring
+        population[:] = offspring
+
+        # Append the current generation statistics to the logbook
+        record = stats.compile(population) if stats else {}
+        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        if verbose:
+            print logbook.stream
+
+    return population, logbook
+
+
 def merge_two_dicts(x, y):
     """Given two dicts, merge them into a new dict as a shallow copy."""
     z = x.copy()
@@ -69,6 +123,7 @@ class fitnessFunc():
         self.tmpdir = tempdir
         self.verbose = verbose
         self.summary = summary
+        self.overwrite = overwrite
         ''' if only post-injector optimisation'''
         # post_injector = True
         self.post_injector = False
@@ -129,8 +184,9 @@ class fitnessFunc():
                 del self.framework.fileSettings['injector400']
             else:
                 self.framework.astra.createInitialDistribution(npart=self.npart, charge=250)
-            self.framework.createInputFiles()
-            self.framework.runInputFiles()
+            if self.overwrite:
+                self.framework.createInputFiles()
+                self.framework.runInputFiles()
 
             self.beam.read_astra_beam_file(self.dirname+'/S07.4928.001')
             self.beam.slice_length = 0.05e-12
@@ -143,28 +199,34 @@ class fitnessFunc():
             fitp = 100*sigmap/meanp
             fhcfield = self.parameters['fhcfield']
             peakI, peakIMomentumSpread, peakIEmittanceX, peakIEmittanceY, peakIMomentum = self.beam.sliceAnalysis()
+            chirp = self.beam.chirp
+            print 'momentum_spread = ', fitp
             constraintsList = {
-                'peakI': {'type': 'greaterthan', 'value': abs(peakI), 'limit': 400, 'weight': 100},
-                'peakIMomentumSpread': {'type': 'lessthan', 'value': peakIMomentumSpread, 'limit': 0.05, 'weight': 3},
-                'peakIEmittanceX': {'type': 'lessthan', 'value': 1e6*peakIEmittanceX, 'limit': 0.5, 'weight': 2.5},
-                'peakIEmittanceY': {'type': 'lessthan', 'value': 1e6*peakIEmittanceY, 'limit': 0.5, 'weight': 2.5},
-                'peakIMomentum': {'type': 'greaterthan','value': 1e-6*peakIMomentum, 'limit': 240, 'weight': 10},
+                'peakI_min': {'type': 'greaterthan', 'value': abs(peakI), 'limit': 400, 'weight': 25},
+                'peakI_max': {'type': 'lessthan', 'value': abs(peakI), 'limit': 600, 'weight': 100},
+                'peakIMomentumSpread': {'type': 'lessthan', 'value': peakIMomentumSpread, 'limit': 0.2, 'weight': 10},
+                'peakIEmittanceX': {'type': 'lessthan', 'value': 1e6*peakIEmittanceX, 'limit': 0.5, 'weight': 10},
+                'peakIEmittanceY': {'type': 'lessthan', 'value': 1e6*peakIEmittanceY, 'limit': 0.5, 'weight': 10},
+                'peakIMomentum': {'type': 'equalto','value': 1e-6*peakIMomentum, 'limit': 240, 'weight': 20},
                 'linac fields': {'type': 'lessthan', 'value': 1e-6*self.linacfields, 'limit': 32, 'weight': 100},
                 '4hc field': {'type': 'lessthan', 'value': 1e-6*fhcfield, 'limit': 35, 'weight': 100},
-                'horizontal emittance': {'type': 'lessthan', 'value': emitx, 'limit': 0.75, 'weight': 4},
-                'vertical emittance': {'type': 'lessthan', 'value': emity, 'limit': 0.75, 'weight': 4},
+                'horizontal emittance': {'type': 'lessthan', 'value': emitx, 'limit': 0.75, 'weight': 10},
+                'vertical emittance': {'type': 'lessthan', 'value': emity, 'limit': 0.75, 'weight': 10},
+                'momentum_spread': {'type': 'lessthan', 'value': fitp, 'limit': 0.2, 'weight': 10},
+                # 'chirp': {'type': 'lessthan', 'value': chirp, 'limit': 1, 'weight': 1}
             }
-            self.twiss.read_astra_emit_files(self.dirname+'/S07.Zemit.001')
-            constraintsList5 = {
-                'last_exn_5': {'type': 'lessthan', 'value': 1e6*self.twiss['enx'], 'limit': 0.75, 'weight': 1},
-                'last_eyn_5': {'type': 'lessthan', 'value': 1e6*self.twiss['eny'], 'limit': 0.75, 'weight': 1},
-            }
-            constraintsList = merge_two_dicts(constraintsList, constraintsList5)
+            # self.twiss.read_astra_emit_files(self.dirname+'/S07.Zemit.001')
+            # constraintsList5 = {
+            #     'last_exn_5': {'type': 'lessthan', 'value': 1e6*self.twiss['enx'], 'limit': 0.75, 'weight': 1},
+            #     'last_eyn_5': {'type': 'lessthan', 'value': 1e6*self.twiss['eny'], 'limit': 0.75, 'weight': 1},
+            # }
+            # constraintsList = merge_two_dicts(constraintsList, constraintsList5)
             fitness = self.cons.constraints(constraintsList)
             if self.verbose:
                 print self.cons.constraintsList(constraintsList)
             if self.summary:
-                self.astra.createHDF5Summary(reference='Longitudinal_GA')
+                np.save('summary_constraints.txt', self.cons.constraintsList(constraintsList))
+                # self.astra.createHDF5Summary(reference='Longitudinal_GA')
             return fitness
         except Exception as e:
             print(e)
@@ -198,19 +260,20 @@ parameters.append(framework.getElement('CLA-L4H-CAV', 'field_amplitude'))
 parameters.append(framework.getElement('CLA-L4H-CAV', 'phase'))
 parameters.append(framework.getElement('CLA-L04-CAV', 'field_amplitude'))
 parameters.append(framework.getElement('CLA-L04-CAV', 'phase'))
-parameters.append(framework.fileSettings['VBC']['groups']['VBC']['dipoleangle'])
+# parameters.append(framework.fileSettings['VBC']['groups']['VBC']['dipoleangle'])
+parameters.append(0.106)
 best = parameters
 
-# results = []
-# with open('longitudinal_best_Short_240_solutions.csv', 'r') as csvfile:
-#   reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
-#   for row in reader:
-#     results.append(row)
-# best = results[0]
+results = []
+with open('longitudinal_best_solutions_running.csv', 'r') as csvfile:
+  reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+  for row in reader:
+    results.append(row)
+best = results[0]
 
 print 'starting values = ', best
 
-# print optfunc(best, dir=os.getcwd()+'/best_longitudinal', scaling=3, overwrite=True, verbose=True, summary=False)
+# print optfunc(best, dir=os.getcwd()+'/best_longitudinal', scaling=6, overwrite=False, verbose=True, summary=True)
 # exit()
 
 
@@ -249,6 +312,7 @@ toolbox.register("select", tools.selTournament, tournsize=3)
 
 
 if __name__ == "__main__":
+    global hof
     random.seed(64)
 
     # Process Pool of 4 workers
@@ -260,7 +324,7 @@ if __name__ == "__main__":
     # toolbox.register("map", futures.map)
 
     if not os.name == 'nt':
-        pop = toolbox.population(n=12)
+        pop = toolbox.population(n=48)
     else:
         pop = toolbox.population(n=6)
     hof = tools.HallOfFame(10)
@@ -270,22 +334,26 @@ if __name__ == "__main__":
     stats.register("min", np.min)
     stats.register("max", np.max)
 
-    pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=50,
-                            stats=stats, halloffame=hof)
+    pop, logbook = eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=50,
+                            stats=stats, halloffame=hof, verbose=True)
 
+    pool.close()
     # print 'pop = ', pop
     print logbook
     print hof
 
     try:
         print 'best fitness = ', optfunc(hof[0], dir=os.getcwd()+'/best_longitudinal', scaling=6, overwrite=True, verbose=True, summary=True)
-        with open('longitudinal_best_Short_240/longitudinal_best_solutions.csv','wb') as out:
+        with open('best_longitudinal/longitudinal_best_solutions.csv','wb') as out:
             csv_out=csv.writer(out)
             for row in hof:
+                csv_out.writerow(row)
+        with open('best_longitudinal/longitudinal_best_stats.csv','wb') as out:
+            csv_out=csv.writer(out)
+            for row in stats:
                 csv_out.writerow(row)
     except:
-        with open('longitudinal_best_Short_240_solutions.csv.tmp','wb') as out:
+        with open('longitudinal_best_solutions.csv.tmp','wb') as out:
             csv_out=csv.writer(out)
             for row in hof:
                 csv_out.writerow(row)
-    pool.close()
