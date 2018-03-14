2e0af91a1d4fc3feb4c620377fad90993942cbe7
James Jones <james.jones@stfc.ac.uk>
2018-03-14 22:56:08 +0000

    Split Framework code-wise

--- ASTRAFramework/ASTRAGeneral/General_clara_240.py
+++ ASTRAFramework/ASTRAGeneral/General_clara_240.py
@@ -1,8 +1,8 @@
 from Framework import *
-import numpy as np
-import read_beam_file as rbf
-
-beam = rbf.beam()
+# import numpy as np
+# import read_beam_file as rbf
+#
+# beam = rbf.beam()
 
 scaling = 4
 
@@ -12,25 +12,9 @@ grids = getGrids(npart=npart)
 # print grids.getGridSizes()
 
 
-framework = Framework('2', overwrite=True)
+framework = Framework('C2V', overwrite=True)
 
-framework.loadSettings('clara400_v12.def')
+framework.loadSettings('../../MasterLattice/Lattices/CLA400-C2V-SP1.def')
 framework.astra.createInitialDistribution(npart=npart,charge=250)
-
-gun = framework.getElement('CLA-HRG1-GUN-CAV')
-# gun['phase'] = -5
-linac = framework.getElement('CLA-L01-CAV')
-# linac['phase'] = 0
-sol = framework.getElement('CLA-LRG1-GUN-SOL')
-# sol['field_amplitude'] = 0.35
-if not os.name == 'nt':
-    framework.astra.defineASTRACommand(['mpiexec','-np',str(scaling*3),'/opt/ASTRA/astra_MPICH2.sh'])
-    framework.CSRTrack.defineCSRTrackCommand(['/opt/OpenMPI-1.4.3/bin/mpiexec','-n',str(scaling*3),'/opt/CSRTrack/csrtrack_openmpi.sh'])
-    # gun['field_definition']= '/home/jkj62/Data_Files/HRRG_1D_RF.dat'
-    # sol['field_definition']= '/home/jkj62/Data_Files/HRRG_combined_sols_100mm_onaxis.dat'
-    # linac['field_definition']= '/home/jkj62/Data_Files/TWS_S-DL.dat'
-
-beam.read_astra_beam_file('2'+'/S07.4928.001')
-print beam.chirp
-# framework.createInputFiles()
+framework.createInputFiles()
 # framework.runInputFiles()
