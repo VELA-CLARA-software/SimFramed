6772bfd5e8afbb4ee428acf2f1d8367b265b8061
James Jones <james.jones@stfc.ac.uk>
2018-03-16 15:10:19 +0000

    Fixed some bugs
    Former-commit-id: be5b9cb371ff45b3d0a7e619171c113e2af33a06

--- SimulationFramework/Examples/General_clara_240.py
+++ SimulationFramework/Examples/General_clara_240.py
@@ -1,31 +1,19 @@
-# if __name__ == '__main__':
-#     if __package__ is None:
-#         import sys
-#         from os import path
-#         print path.dirname( path.abspath(__file__) )
-#         sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-#         import Framework
-#     else:
-#         from .. import Framework
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname( os.path.abspath(__file__)))))
 from SimulationFramework.Framework import *
-# import numpy as np
-# import read_beam_file as rbf
-#
-# beam = rbf.beam()
 
-scaling = 4
+
+scaling = 3
 
 npart=2**(3*scaling)
 
-grids = getGrids(npart=npart)
-# print grids.getGridSizes()
-
-
 framework = Framework('C2V', overwrite=True)
 
-framework.loadSettings('../../MasterLattice/Lattices/CLA400-C2V-SP1.def')
+''' You need to define the location of your astra command!'''
+''' This is the default for windows...'''
+framework.astra.defineASTRACommand(['../../MasterLattice/ASTRA/astra.exe'])
+
+framework.loadSettings('../../MasterLattice/Lattices/CLA400-C2V-INJ.def')
 framework.astra.createInitialDistribution(npart=npart,charge=250)
 framework.createInputFiles()
-# framework.runInputFiles()
+framework.runInputFiles()
