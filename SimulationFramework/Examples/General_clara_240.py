4d84bdd08d8d910557b28befb8443a8b0a919319
James Jones <james.jones@stfc.ac.uk>
2018-03-24 11:28:47 +0000

    Added Elegant test
    Former-commit-id: 5f1af1551fa5214660e8987d9d5100a3e3923595

--- SimulationFramework/Examples/General_clara_240.py
+++ SimulationFramework/Examples/General_clara_240.py
@@ -5,7 +5,7 @@ import SimulationFramework.Modules.read_beam_file as rbf
 beam = rbf.beam()
 degree = 1./360.*2.*math.pi
 
-scaling = 4
+scaling = 3
 
 npart=2**(3*scaling)
 
@@ -21,8 +21,17 @@ npart=2**(3*scaling)
 
 ''' ############# CLARA 400 #############'''
 
-framework = Framework('CLARA', overwrite=True)
+framework = Framework('CLARA', overwrite=True, clean=False)
+
+ncpu = scaling*3
+
+if not os.name == 'nt':
+    framework.astra.defineASTRACommand(['mpiexec','-np',str(ncpu),'/opt/ASTRA/astra_MPICH2.sh'])
+    framework.CSRTrack.defineCSRTrackCommand(['/opt/OpenMPI-1.4.3/bin/mpiexec','-n',str(ncpu),'/opt/CSRTrack/csrtrack_openmpi.sh'])
+else:
+    framework.astra.defineASTRACommand(['astra'])
+    framework.CSRTrack.defineCSRTrackCommand(['CSRtrack_1.201.wic.exe'])
 
 framework.loadSettings('Lattices/clara400_v12.def')
 framework.astra.createInitialDistribution(npart=npart,charge=250)
-framework.createRunProcessInputFiles(run=True)#, files=['VBC'])
+framework.createRunProcessInputFiles(run=True, files=['S02', 'L02'])
