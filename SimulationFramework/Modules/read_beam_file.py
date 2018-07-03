fe18a92f666f72c579caeabf6863037c55039280
James Jones <james.jones@stfc.ac.uk>
2018-07-03 16:59:26 +0100

    more sdds fixes

--- SimulationFramework/Modules/read_beam_file.py
+++ SimulationFramework/Modules/read_beam_file.py
@@ -3,11 +3,7 @@ import h5py
 import numpy as np
 import scipy.constants as constants
 from scipy.spatial.distance import cdist
-if os.name == 'nt':
-    try:
-        import sdds
-    except:
-        pass
+import sdds
 import read_gdf_file as rgf
 
 class beam(object):
