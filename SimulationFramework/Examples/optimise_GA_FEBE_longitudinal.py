1cda215736e8fb5671204c6b275fc565ea54db0c
James Jones <james.jones@stfc.ac.uk>
2018-07-04 17:49:54 +0100

    D'Oh!
    Former-commit-id: d9b82dda2cf17e58f37a39d06d926267c76cffab

--- SimulationFramework/Examples/optimise_GA_FEBE_longitudinal.py
+++ SimulationFramework/Examples/optimise_GA_FEBE_longitudinal.py
@@ -130,7 +130,7 @@ class fitnessFunc():
             if self.overwrite:
                 startS = self.framework['L02'].startObject['position_start'][2]
                 self.framework['L02'].file_block['input']['prefix'] = '../basefiles_'+str(self.scaling)+'/'
-                # self.framework.track(track=False)#, startfile='FEBE')
+                self.framework.track()#, startfile='FEBE')
 
             # self.beam.read_astra_beam_file(self.dirname+'/S07.4928.001')
             self.beam.read_HDF5_beam_file(self.dirname+'/CLA-FEB-W-FOCUS.hdf5')
@@ -219,8 +219,8 @@ best = parameters
 
 # print 'starting values = ', best
 #
-print optfunc(best, dir=os.getcwd()+'/FEBE_best_longitudinal', scaling=6, overwrite=True, verbose=True, summary=False)
-exit()
+# print optfunc(best, dir=os.getcwd()+'/FEBE_best_longitudinal', scaling=6, overwrite=True, verbose=True, summary=False)
+# exit()
 
 
 # startranges = [[10, 32], [-40,40], [10, 32], [-40,40], [10, 32], [-40,40], [10, 32], [135,200], [10, 32], [-40,40], [0.8,0.15]]
