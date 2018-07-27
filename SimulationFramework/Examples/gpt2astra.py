411e0125af1cb2f76a63c66ec362002630cff924
James Jones <james.jones@stfc.ac.uk>
2018-07-27 09:55:45 +0100

    Tweaks for gpt to astra conversion

---
+++ SimulationFramework/Examples/gpt2astra.py
@@ -0,0 +1,15 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname( os.path.abspath(__file__)))))
+import SimulationFramework.Modules.read_beam_file as rbf
+import argparse
+
+parser = argparse.ArgumentParser(description='Convert GPT GDF beam file to ASTRA format')
+parser.add_argument('inputfile')
+parser.add_argument('outputfile')
+
+args = parser.parse_args()
+
+beam = rbf.beam()
+beam.read_gdf_beam_file(args.inputfile)
+beam.beam['longitudinal_reference'] = 'z'
+beam.write_astra_beam_file(args.outputfile, normaliseZ=True)
