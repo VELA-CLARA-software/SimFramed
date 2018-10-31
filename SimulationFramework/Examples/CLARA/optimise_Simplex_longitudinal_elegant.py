2a7042a94c319e4b5f6d30d1e8c241358dc16cd6
James Jones <james.jones@stfc.ac.uk>
2018-10-31 10:03:31 +0000

    Updates to lattices and examples

--- SimulationFramework/Examples/CLARA/optimise_Simplex_longitudinal_elegant.py
+++ SimulationFramework/Examples/CLARA/optimise_Simplex_longitudinal_elegant.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os, sys
 sys.path.append(os.path.abspath(__file__+'/../../../../'))
-from FitnessFunc_Longitudinal import *
+from FitnessFunc_Longitudinal_elegant import *
 import operator
 import random
 import csv
@@ -10,7 +10,7 @@ import yaml
 from functools import partial
 
 def saveState(args, fitness):
-    with open('best_solutions_running_simplex.csv','a') as out:
+    with open('best_solutions_running_simplex_elegant.csv','a') as out:
         csv_out=csv.writer(out)
         args=list(args)
         args.append(fitness)
@@ -38,7 +38,7 @@ def optfunc(inputargs, dir=None, *args, **kwargs):
     return fitvalue
 
 framework = Framework('longitudinal_best', overwrite=False)
-framework.loadSettings('Lattices/clara400_v12_v3.def')
+framework.loadSettings('Lattices/clara400_v12_v3_elegant.def')
 injparameters = []
 parameters = []
 injparameternames = [['CLA-HRG1-GUN-CAV', 'phase'], ['CLA-HRG1-GUN-SOL', 'field_amplitude'], ['CLA-L01-CAV', 'field_amplitude'],
@@ -55,13 +55,13 @@ for p in injparameternames:
 for p in parameternames:
     parameters.append(framework.getElement(*p))
 
-results = []
-with open('CLARA_longitudinal_best_solutions_simplex.csv.tmp', 'r') as csvfile:
-  reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
-  for row in reader:
-    results.append(row)
-best = results[0]
-best = [22930206.535622053,-19.919884243633238,23009930.128022004,-8.084999503888273,20332245.251669575,156.72188403590755,32399709.39536675,31.949905578189217,0.1882468402535032]
+# results = []
+# with open('CLARA_longitudinal_best_solutions_simplex.csv.tmp', 'r') as csvfile:
+#   reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+#   for row in reader:
+#     results.append(row)
+# best = results[0]
+best = [21937707.15643109,-19.69402630723058,20730148.29623335,-8.085324458606465,27523152.262458388,165.6365900692502,27649139.10995977,34.65396438539781,0.23305265450852905]
 
 print 'starting values = ', best
 
@@ -78,14 +78,15 @@ print 'starting values = ', best
 
 optfunc3 = partial(optfunc, dir=None, scaling=3, post_injector=True)
 optfunc4 = partial(optfunc, dir=None, scaling=4, post_injector=True)
+optfunc5 = partial(optfunc, dir=None, scaling=5, post_injector=True)
 
-with open('best_solutions_running_simplex.csv','w') as out:
+with open('best_solutions_running_simplex_elegant.csv','w') as out:
     pass
-res = minimize(optfunc3, best, method='nelder-mead', options={'xtol': 1e-3, 'disp': True})
+res = minimize(optfunc4, best, method='nelder-mead', options={'xtol': 1e-3, 'disp': True})
 print res
 
 try:
-    print 'best fitness = ', optfunc(res.x, dir=os.getcwd()+'/CLARA_best_longitudinal_simplex_2', scaling=6, overwrite=True, verbose=True, summary=True, post_injector=True)
+    print 'best fitness = ', optfunc(res.x, dir=os.getcwd()+'/CLARA_best_longitudinal_simplex_elegant', scaling=6, overwrite=True, verbose=True, summary=True, post_injector=True)
 except:
     pass
 
