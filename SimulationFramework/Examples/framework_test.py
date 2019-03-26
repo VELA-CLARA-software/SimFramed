29cbadb6dd8313d1d07bdb12119d90313ee5645c
James Jones <james.jones@stfc.ac.uk>
2019-03-26 14:53:50 +0000

    Massive mess-up in elegant createDrifts - now fixed!
    Former-commit-id: 19853d33984cbcfb9475572fdad06dc6241f4ba3

--- SimulationFramework/Examples/framework_test.py
+++ SimulationFramework/Examples/framework_test.py
@@ -17,7 +17,7 @@ import numpy as np
 # lattice.track()#startfile='C2V')
 
 lattice = fw.Framework('example', clean=True, verbose=True)
-lattice.loadSettings('Lattices/clara400_v12_v3.def')
+lattice.loadSettings('Lattices/clara400_v12_v3_elegant_jkj.def')
 if not os.name == 'nt':
     scaling = 5
     lattice.defineASTRACommand(['mpiexec','-np',str(3*scaling),'/opt/ASTRA/astra_MPICH2.sh'])
@@ -29,7 +29,6 @@ else:
     lattice.generator.number_of_particles = 2**(3*5)
 lattice.defineElegantCommand(['elegant'])
 
-lattice['S02'].prefix = '../CLARA/basefiles_4/'
 oldl02grad = lattice.getElement('CLA-L02-CAV', 'field_amplitude')
 oldl02phase = lattice.getElement('CLA-L02-CAV', 'phase')
 oldl03grad = lattice.getElement('CLA-L03-CAV', 'field_amplitude')
@@ -38,11 +37,11 @@ oldl04grad = lattice.getElement('CLA-L04-CAV', 'field_amplitude')
 oldl04phase = lattice.getElement('CLA-L04-CAV', 'phase')
 oldl0xgrad = lattice.getElement('CLA-L4H-CAV', 'field_amplitude')
 oldl0xphase = lattice.getElement('CLA-L4H-CAV', 'phase')
-lattice.modifyElement('CLA-L02-CAV', 'field_amplitude', np.sqrt(2)*11.25e6 )
-lattice.modifyElement('CLA-L03-CAV', 'field_amplitude', np.sqrt(2)*11.25e6 )
-lattice.modifyElement('CLA-L04-CAV', 'field_amplitude', np.sqrt(2)*12e6 )
-lattice.modifyElement('CLA-L4H-CAV', 'field_amplitude', np.sqrt(2)*15.75e6 )
-lattice.modifyElement('CLA-L02-CAV', 'phase', oldl02phase + 5.0)
+# lattice.modifyElement('CLA-L02-CAV', 'field_amplitude', np.sqrt(2)*11.25e6 )
+# lattice.modifyElement('CLA-L03-CAV', 'field_amplitude', np.sqrt(2)*11.25e6 )
+# lattice.modifyElement('CLA-L04-CAV', 'field_amplitude', np.sqrt(2)*12e6 )
+# lattice.modifyElement('CLA-L4H-CAV', 'field_amplitude', np.sqrt(2)*15.75e6 )
+# lattice.modifyElement('CLA-L02-CAV', 'phase', oldl02phase + 5.0)
 # lattice.save_changes_file()
 
 # print lattice['S02'].prefix
@@ -52,10 +51,12 @@ lattice.modifyElement('CLA-L02-CAV', 'phase', oldl02phase + 5.0)
 # lattice['CLA-S02-MAG-QUAD-01'].k1 = 13.15
 # print lattice['S02'].getElement('CLA-S02-MAG-QUAD-01','k1')
 # # lattice.track(files=['generator', 'injector400'], track=True, postprocess=True)
+lattice['POSTINJ'].prefix = '../CLARA/basefiles_4/'
 lattice.change_Lattice_Code('All','elegant')
+lattice.track(files=['POSTINJ'])
 # # lattice.track(startfile='S02')#,preprocess=True, track=False, postprocess=False)
 # lattice['bunch_compressor'].set_angle(-0.0)
-lattice.track(startfile='S02', endfile='S07')#,preprocess=True, track=False, postprocess=False)
+# lattice.track(startfile='S02', endfile='S07')#,preprocess=True, track=False, postprocess=False)
 
 # exit()
 # def f(ph):
