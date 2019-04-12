d17020c80490fe61595a316177351f06a830467b
James Jones <james.jones@stfc.ac.uk>
2019-04-12 18:04:33 +0100

    FEBE Changes

--- SimulationFramework/Examples/FEBE/FEBE_transverse_upto_S07F.py
+++ SimulationFramework/Examples/FEBE/FEBE_transverse_upto_S07F.py
@@ -13,7 +13,7 @@ names = framework.getElementType('quadrupole','objectname')
 index1 = names.index('CLA-S07-MAG-QUAD-01')
 index2 = names.index('CLA-S07-MAG-QUAD-10')+1
 index3 = names.index('CLA-FEB-MAG-QUAD-13')
-parameter_names = [q for q in names[index1:index2]]
+parameter_names = [q for q in names[:index2]]
 # parameter_names.append('CLA-FEB-MAG-QUAD-01')
 # parameter_names.append('CLA-FEB-MAG-QUAD-02')
 # parameter_names += [q for q in names[index3:]]
@@ -27,16 +27,22 @@ print parameter_names
 # #1.08184, -2.38876, 1.66352, -1.29897, 2.07599, -1.95901, -1.73588, 1.63013, -0.154276, -0.048223, 0.0478511, -0.0479035, 0.0472847]
 # best = best + parameters[index3:]
 #
-with open('S07_transverse_best_changes.yaml', 'r') as infile:
+with open('transverse_best_changes_upto_S07.yaml', 'r') as infile:
     data = dict(yaml.load(infile, Loader=yaml.UnsafeLoader))
-    # data=[]
-    # best = [data[n]['k1l'] for n in parameter_names]
-    best = []
+    bestd = {}
     for n in parameter_names:
         if n in data:
-            best.append(data[n]['k1l'])
+            bestd[n] = data[n]['k1l']
         else:
-            best.append(framework[n]['k1l'])
+            bestd[n] = framework[n]['k1l']
+
+# with open('S07_transverse_best_changes.yaml', 'r') as infile:
+#     data = dict(yaml.load(infile, Loader=yaml.UnsafeLoader))
+#     for n in parameter_names:
+#         if n in data:
+#             bestd[n] = data[n]['k1l']
+
+best = [bestd[n] for n in parameter_names]
 
 class FEBE_Transverse(Optimise_transverse):
 
@@ -46,10 +52,8 @@ class FEBE_Transverse(Optimise_transverse):
         index1 = names.index('CLA-S07-MAG-QUAD-01')
         index2 = names.index('CLA-S07-MAG-QUAD-10') + 1
         index3 = names.index('CLA-FEB-MAG-QUAD-13')
-        self.parameter_names = []
         self.parameters = []
-        self.parameter_names = [q for q in names[index1:index2]]
-        self.parameters = [[q, 'k1l'] for q in names[index1:index2]]
+        self.parameters = [[q, 'k1l'] for q in names[:index2]]
         # self.parameter_names.append('CLA-FEB-MAG-QUAD-01')
         # self.parameters.append(['FODO_F', 'k1l'])
         # self.parameter_names.append('CLA-FEB-MAG-QUAD-02')
@@ -57,7 +61,7 @@ class FEBE_Transverse(Optimise_transverse):
         # self.parameter_names += [q for q in names[index3:]]
         # self.parameters += [[q, 'k1l'] for q in names[index3:]]
         self.base_files = '../../../CLARA/basefiles_' + str(int(scaling)) + '/'
-        self.best_changes = './S07_transverse_best_changes.yaml'
+        self.best_changes = './transverse_best_changes_upto_S07.yaml'
         self.start_file = 'PreFEBE'
 
     def calculateBeamParameters(self):
@@ -72,19 +76,26 @@ class FEBE_Transverse(Optimise_transverse):
         quadlengths = self.framework.getElementType('quadrupole','length')
         quadnames = self.framework.getElementType('quadrupole','objectname')
 
-        # twiss.read_elegant_twiss_files( [ self.dirname+'/'+n+'.twi' for n in ['S07', 'S07F']])
+        constraintsListQuads = {
+            'max_k': {'type': 'lessthan', 'value': [abs(k) for k, l, n in zip(quadkls, quadlengths, quadnames) if not 'FEBE' in n], 'limit': 1.0, 'weight': 300},
+
+        }
+        # print self.cons.constraintsList(constraintsListQuads)
+        # constraintsList = merge_two_dicts(constraintsList, constraintsListQuads)
+
+        # twiss.read_elegant_twiss_files( [ self.dirname+'/'+n+'.twi' for n in ['S02', 'L02', 'S03', 'L03', 'S04', 'L4H', 'S05', 'S06', 'L04', 'S07', 'S07F']])
         twiss.read_elegant_twiss_files( self.dirname+'/'+self.start_file+'.twi' )
         constraintsListSigmas = {
             'max_xrms': {'type': 'lessthan', 'value': max(1e3*twiss['sigma_x']), 'limit': 1, 'weight': 5},
             'max_yrms': {'type': 'lessthan', 'value': max(1e3*twiss['sigma_y']), 'limit': 1, 'weight': 5},
-            'min_xrms': {'type': 'greaterthan', 'value': min(1e3*twiss['sigma_x']), 'limit': 0.05, 'weight': 50},
-            'min_yrms': {'type': 'greaterthan', 'value': min(1e3*twiss['sigma_y']), 'limit': 0.05, 'weight': 50},
+            'min_xrms': {'type': 'greaterthan', 'value': min(1e3*twiss['sigma_x']), 'limit': 0.025, 'weight': 5},
+            'min_yrms': {'type': 'greaterthan', 'value': min(1e3*twiss['sigma_y']), 'limit': 0.025, 'weight': 5},
             'last_exn': {'type': 'lessthan', 'value': 1e6*twiss['enx'][-1], 'limit': 2, 'weight': 0},
             'last_eyn': {'type': 'lessthan', 'value': 1e6*twiss['eny'][-1], 'limit': 0.75, 'weight': 0},
-            'max_betax': {'type': 'lessthan', 'value': max(twiss['beta_x']), 'limit': 250, 'weight': 5},
-            'max_betay': {'type': 'lessthan', 'value': max(twiss['beta_y']), 'limit': 250, 'weight': 5},
-            # 'max_alphax': {'type': 'lessthan', 'value': max(abs(twiss['alpha_x'])), 'limit': 10, 'weight': 5},
-            # 'max_alphay': {'type': 'lessthan', 'value': max(abs(twiss['alpha_y'])), 'limit': 10, 'weight': 5},
+            'max_betax': {'type': 'lessthan', 'value': max(twiss['beta_x']), 'limit': 100, 'weight': 15},
+            'max_betay': {'type': 'lessthan', 'value': max(twiss['beta_y']), 'limit': 100, 'weight': 15},
+            # 'max_alphax': {'type': 'lessthan', 'value': max(abs(twiss['alpha_x'])), 'limit': 20, 'weight': 5},
+            # 'max_alphay': {'type': 'lessthan', 'value': max(abs(twiss['alpha_y'])), 'limit': 20, 'weight': 5},
         }
         # print self.cons.constraintsList(constraintsListSigmas)
         constraintsList = merge_two_dicts(constraintsList, constraintsListSigmas)
@@ -101,14 +112,14 @@ class FEBE_Transverse(Optimise_transverse):
 
         # twiss.read_elegant_twiss_files( self.dirname+'/S07F.twi' )
         constraintsListFEBEStart = {
-            'betax': {'type': 'equalto', 'value': twiss['beta_x'][-1], 'limit': 0.706961, 'weight': 50},
-            'betay': {'type': 'equalto', 'value': twiss['beta_y'][-1], 'limit': 3.94176, 'weight': 50},
-            'alphax': {'type': 'equalto', 'value': twiss['alpha_x'][-1], 'limit': -1.40098, 'weight': 50},
-            'alphay': {'type': 'equalto', 'value': twiss['alpha_y'][-1], 'limit': 0.872449, 'weight': 50},
+            'betax': {'type': 'equalto', 'value': twiss['beta_x'][-1], 'limit': 0.706961, 'weight': 5},
+            'betay': {'type': 'equalto', 'value': twiss['beta_y'][-1], 'limit': 3.94176, 'weight': 5},
+            'alphax': {'type': 'equalto', 'value': twiss['alpha_x'][-1], 'limit': -1.40098, 'weight': 5},
+            'alphay': {'type': 'equalto', 'value': twiss['alpha_y'][-1], 'limit': 0.872449, 'weight': 5},
             # 'etax': {'type': 'equalto', 'value': twiss.elegant['etax'][-1], 'limit': 0., 'weight': 500},
             # 'etaxp': {'type': 'equalto', 'value': twiss.elegant['etaxp'][-1], 'limit': 0., 'weight': 500},
         }
-        print self.cons.constraintsList(constraintsListFEBEStart)
+        # print self.cons.constraintsList(constraintsListFEBEStart)
         constraintsList = merge_two_dicts(constraintsList, constraintsListFEBEStart)
 
         fitness = self.cons.constraints(constraintsList)
@@ -116,10 +127,9 @@ class FEBE_Transverse(Optimise_transverse):
             print self.cons.constraintsList(constraintsList)
         return fitness
 
-
 if __name__ == "__main__":
         fit = FEBE_Transverse('./FEBE_Single.def', scaling=6)
-        fit.setChangesFile(['./simplex_best_changes.yaml','./transverse_best_changes_upto_S07.yaml'])
+        fit.setChangesFile(['./simplex_best_changes.yaml'])
         fit.verbose = False
-        fit.Nelder_Mead(best, step=0.01)
+        fit.Nelder_Mead(best, step=0.05)
         # fit.Simplex(best)
