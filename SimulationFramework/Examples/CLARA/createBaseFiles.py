e28ed9a8fe193c646594bb137b8c6cc080042b70
James Jones <james.jones@stfc.ac.uk>
2019-07-09 15:02:20 +0100

    Update createBaseFiles.py

--- SimulationFramework/Examples/CLARA/createBaseFiles.py
+++ SimulationFramework/Examples/CLARA/createBaseFiles.py
@@ -11,14 +11,13 @@ def create_base_files(scaling):
     framework = Framework('basefiles_'+str(scaling), overwrite=True)
     framework.loadSettings('Lattices/clara400_v12_v3.def')
     if not os.name == 'nt':
-        framework.defineASTRACommand(['mpiexec','-np',str(4*scaling),'/opt/ASTRA/astra_MPICH2.sh'])
-        framework.defineGeneratorCommand(['/opt/ASTRA/generator.sh'])
-        framework.defineCSRTrackCommand(['/opt/OpenMPI-1.4.3/bin/mpiexec','-n',str(4*scaling),'/opt/CSRTrack/csrtrack_openmpi.sh'])
+        framework.defineASTRACommand(scaling=scaling)
+        framework.defineCSRTrackCommand(scaling=scaling)
     framework.generator.number_of_particles = 2**(3*scaling)
     # framework.change_Lattice_Code('VBC', 'ASTRA')
     framework.track()#startfile='VBC')
 
 ## Modify as appropriate! ##
-for i in [4]:
+for i in [4,5,6]:
     create_base_files(i)
 exit()
