81cff02aaa177899a2d4a339ba63967887010fc5
James Jones <james.jones@stfc.ac.uk>
2019-07-09 14:58:22 +0100

    Convert FEBE to ClassFiles optimisation

--- SimulationFramework/Examples/FEBE/FEBE_nelder_mead.py
+++ SimulationFramework/Examples/FEBE/FEBE_nelder_mead.py
@@ -1,7 +1,7 @@
 import sys, os
 import numpy as np
 sys.path.append('./../../../')
-from SimulationFramework.Examples.CLARA.Elegant.Optimise_longitudinal_Elegant import Optimise_Elegant
+from SimulationFramework.ClassFiles.Optimise_longitudinal_Elegant import Optimise_Elegant
 from SimulationFramework.Modules.merge_two_dicts import merge_two_dicts
 from functools import partial
 from ruamel import yaml
@@ -20,48 +20,28 @@ class FEBE(Optimise_Elegant):
         self.parameter_names.append(['FODO_F', 'k1l'])
         self.parameter_names.append(['FODO_D', 'k1l'])
         self.scaling = 6
-        CLARA_dir = os.path.relpath(__file__+'/../../CLARA/')
-        self.optfunc = partial(self.OptimisingFunction, scaling=self.scaling, post_injector=self.POST_INJECTOR, verbose=False,
-            basefiles='../../../CLARA/basefiles_'+str(self.scaling)+'/', CLARA_dir=CLARA_dir, sample_interval=2**(3*1))
-        self.verbose = False
-
-    def load_best(self, filename):
-        with open(filename, 'r') as infile:
-            data = dict(yaml.load(infile, Loader=yaml.UnsafeLoader))
-            # best = [data[n]['k1l'] for n in parameter_names]
-            best = []
-            for n, p in self.parameter_names:
-                if n in data:
-                    best.append(data[n][p])
-                elif n == 'bunch_compressor' and p == 'set_angle':
-                    best.append(data['CLA-VBC-MAG-DIP-01']['angle'])
-                else:
-                    print(n, p)
-                    if not hasattr(self, 'framework'):
-                        self.framework = fw.Framework(None)
-                        self.framework.loadSettings(self.lattice)
-                    best.append(self.framework[n][p])
-            self.best = best
+        self.sample_interval = 2**(3*0)
+        self.base_files = '../../../CLARA/basefiles_'+str(self.scaling)+'/'
+        self.clean = True
+        self.doTracking = True
 
     def calculate_constraints(self):
-        twiss = self.fit.twiss
-        self.beam = self.fit.beam
         constraintsList = {}
-        quadkls = self.fit.framework.getElementType('quadrupole','k1l')
-        quadlengths = self.fit.framework.getElementType('quadrupole','length')
-        quadnames = self.fit.framework.getElementType('quadrupole','objectname')
+        quadkls = self.framework.getElementType('quadrupole','k1l')
+        quadlengths = self.framework.getElementType('quadrupole','length')
+        quadnames = self.framework.getElementType('quadrupole','objectname')
 
-        self.beam.read_HDF5_beam_file(self.fit.dirname+'/CLA-FEB-W-FOCUS-01.hdf5')
+        self.beam.read_HDF5_beam_file(self.dirname+'/CLA-FEB-FOCUS-01.hdf5')
         self.beam.slice_length = 0.05e-12
 
         t = 1e12*(self.beam.t-np.mean(self.beam.t))
-        t_grid = np.linspace(min(t), max(t), 2**8)
+        t_grid = np.linspace(min(t), max(t), 2**10)
         peakIPDF = self.beam.PDF(t, t_grid, bandwidth=self.beam.rms(t)/(2**4))
         peakICDF = self.beam.CDF(t, t_grid, bandwidth=self.beam.rms(t)/(2**4))
-        peakIFWHM, indexes = self.beam.FWHM(t_grid, peakIPDF, frac=0.1)
-        peakIFWHM2, indexes2 = self.beam.FWHM(t_grid, peakIPDF, frac=3)
-        stdpeakIPDF = (max(peakIPDF[indexes2]) - min(peakIPDF[indexes2]))/np.mean(peakIPDF[indexes2])
-        print('stdpeakIPDF = ', stdpeakIPDF)
+        peakIFWHM, indexes = self.beam.FWHM(t_grid, peakIPDF, frac=4)
+        # peakIFWHM2, indexes2 = self.beam.FWHM(t_grid, peakIPDF, frac=10)
+        # stdpeakIPDF = (max(peakIPDF[indexes2]) - min(peakIPDF[indexes2]))/np.mean(peakIPDF[indexes2])
+        # print('stdpeakIPDF = ', stdpeakIPDF)
         # print 'Peak Fraction = ', 100*peakICDF[indexes][-1]-peakICDF[indexes][0], peakICDF[indexes][-1], peakICDF[indexes][0]
         #
         # import matplotlib.pyplot as plt
@@ -87,22 +67,22 @@ class FEBE(Optimise_Elegant):
 
         linac_fields = np.array([1e-6*self.linac_fields[i] for i in [0,1,3]])
 
-        twiss.read_elegant_twiss_files( self.fit.dirname+'/FEBE.twi' )
-        ipindex = list(twiss.elegant['ElementName']).index('CLA-FEB-W-FOCUS-01')
+        self.twiss.read_elegant_twiss_files( self.dirname+'/FEBE.twi' )
+        ipindex = list(self.twiss.elegant['ElementName']).index('CLA-FEB-FOCUS-01')
         constraintsListFEBE = {
-            # 'ip_enx': {'type': 'lessthan', 'value': 1e6*twiss.elegant['enx'][ipindex], 'limit': 2, 'weight': 0},
-            # 'ip_eny': {'type': 'lessthan', 'value': 1e6*twiss.elegant['eny'][ipindex], 'limit': 0.5, 'weight': 2.5},
+            # 'ip_enx': {'type': 'lessthan', 'value': 1e6*self.twiss.elegant['enx'][ipindex], 'limit': 2, 'weight': 0},
+            # 'ip_eny': {'type': 'lessthan', 'value': 1e6*self.twiss.elegant['eny'][ipindex], 'limit': 0.5, 'weight': 2.5},
             'field_max': {'type': 'lessthan', 'value': linac_fields, 'limit': 32, 'weight': 300},
-            'momentum_max': {'type': 'lessthan', 'value': 0.511*twiss.elegant['pCentral0'][ipindex], 'limit': 250, 'weight': 250},
-            'momentum_min': {'type': 'greaterthan', 'value': 0.511*twiss.elegant['pCentral0'][ipindex], 'limit': 240, 'weight': 150},
+            'momentum_max': {'type': 'lessthan', 'value': 0.511*self.twiss.elegant['pCentral0'][ipindex], 'limit': 250, 'weight': 250},
+            'momentum_min': {'type': 'greaterthan', 'value': 0.511*self.twiss.elegant['pCentral0'][ipindex], 'limit': 240, 'weight': 150},
             # 'peakI_min': {'type': 'greaterthan', 'value': abs(peakI), 'limit': 950, 'weight': 20},
             # 'peakI_max': {'type': 'lessthan', 'value': abs(peakI), 'limit': 1050, 'weight': 20},
             # 'peakIMomentumSpread': {'type': 'lessthan', 'value': peakIMomentumSpread, 'limit': 0.1, 'weight': 2},
-            'peakIEmittanceX': {'type': 'lessthan', 'value': 1e6*peakIEmittanceX, 'limit': 3, 'weight': 15},
+            'peakIEmittanceX': {'type': 'lessthan', 'value': 1e6*peakIEmittanceX, 'limit': 5, 'weight': 15},
             'peakIEmittanceY': {'type': 'lessthan', 'value': 1e6*peakIEmittanceY, 'limit': 0.75, 'weight': 1.5},
-            'peakIFWHM': {'type': 'lessthan','value': peakIFWHM, 'limit': 0.15, 'weight': 100},
-            'stdpeakIFWHM': {'type': 'lessthan','value': stdpeakIPDF, 'limit': 1, 'weight': 500},
-            'peakIFraction': {'type': 'greaterthan','value': 100*peakICDF[indexes][-1]-peakICDF[indexes][0], 'limit': 75, 'weight': 20},
+            'peakIFWHM': {'type': 'lessthan','value': peakIFWHM, 'limit': 0.05, 'weight': 100},
+            # 'stdpeakIFWHM': {'type': 'lessthan','value': stdpeakIPDF, 'limit': 1, 'weight': 0},
+            'peakIFraction': {'type': 'greaterthan','value': 100*peakICDF[indexes][-1]-peakICDF[indexes][0], 'limit': 75, 'weight': 200},
         }
         constraintsList = merge_two_dicts(constraintsList, constraintsListFEBE)
 
@@ -111,10 +91,11 @@ class FEBE(Optimise_Elegant):
             print(self.cons.constraintsList(constraintsList))
         return constraintsList
 
-opt = FEBE()
-opt.set_changes_file(['./nelder_mead_best_changes.yaml', './transverse_best_changes_upto_S07.yaml', './S07_transverse_best_changes.yaml', './FEBE_transverse_best_changes.yaml'])
-opt.set_lattice_file('./FEBE_Single.def')
-opt.set_start_file('PreFEBE')
-opt.load_best('nelder_mead_best_changes.yaml')
-opt.Nelder_Mead(step=[1e6, 1, 1e6, 1, 1e6, 1, 1e6, 1, 0.025, 0.05, 0.1, 0.1])
-# opt.Simplex()
+if __name__ == "__main__":
+    opt = FEBE()
+    opt.set_changes_file(['./nelder_mead_best_changes.yaml', './transverse_best_changes_upto_S07.yaml', './S07_transverse_best_changes.yaml', './FEBE_transverse_best_changes.yaml'])
+    opt.set_lattice_file('./FEBE_Single.def')
+    opt.set_start_file('PreFEBE')
+    opt.load_best('./nelder_mead_best_changes.yaml')
+    opt.Nelder_Mead(step=[5e6, 5, 5e6, 5, 5e6, 5, 5e6, 5, 0.05, 0.1, 0.5, 0.5])
+    # opt.Simplex()
--- SimulationFramework/Examples/FEBE/FEBE_test.py
+++ SimulationFramework/Examples/FEBE/FEBE_test.py
@@ -1,23 +1,31 @@
 import sys, os
 sys.path.append('../../../')
-import SimulationFramework.Framework as fw
+from SimulationFramework.ClassFiles.Optimise_longitudinal_Elegant import Optimise_Elegant
 import SimulationFramework.Modules.read_twiss_file as rtf
 import numpy as np
 
-lattice = fw.Framework('example', clean=False, verbose=True)
-lattice.loadSettings('FEBE_Single.def')
-if not os.name == 'nt':
-    scaling = 5
-    lattice.defineASTRACommand(['mpiexec','-np',str(3*scaling),'/opt/ASTRA/astra_MPICH2.sh'])
-    # lattice.defineASTRACommand(['/opt/ASTRA/astra.sh'])
-    lattice.defineGeneratorCommand(['/opt/ASTRA/generator.sh'])
-    lattice.defineCSRTrackCommand(['/opt/OpenMPI-1.4.3/bin/mpiexec','-n',str(3*scaling),'/opt/CSRTrack/csrtrack_openmpi.sh'])
-    lattice.generator.number_of_particles = 2**(3*scaling)
-else:
-    lattice.generator.number_of_particles = 2**(3*5)
-lattice.defineElegantCommand(['elegant'])
-lattice.change_Lattice_Code('All','elegant')
-lattice['PreFEBE'].prefix = '../../CLARA/basefiles_6/'
-lattice['PreFEBE'].sample_interval = 2**(3*4)
-lattice.load_changes_file(['./nelder_mead_best_changes.yaml', './transverse_best_changes_upto_S07.yaml', './S07_transverse_best_changes.yaml', './FEBE_transverse_best_changes.yaml'])
-lattice.track(startfile='PreFEBE')
+class FEBE(Optimise_Elegant):
+
+    # injector_startingvalues = [-9.,0.345,2.1e7,-16.,0.052500000000000005,-0.05]
+    # startingvalues = best = np.array([ 3.13845650e+07, -2.33062481e+01,  2.96752546e+07, -3.41502595e+00,
+    #     2.74842883e+07,  1.87482967e+02,  3.11918859e+07,  5.07160187e+01,
+    #    -1.22393267e-01,  6.12784140e-01])
+
+    def __init__(self):
+        super(FEBE, self).__init__()
+        self.parameter_names.append(['FODO_F', 'k1l'])
+        self.parameter_names.append(['FODO_D', 'k1l'])
+        self.scaling = 6
+        self.sample_interval = 2**(3*1)
+        self.base_files = '../../CLARA/basefiles_'+str(self.scaling)+'/'
+        self.clean = True
+        self.doTracking = True
+
+if __name__ == "__main__":
+    opt = FEBE()
+    opt.set_changes_file(['./transverse_best_changes_upto_S07.yaml', './S07_transverse_best_changes.yaml', './FEBE_transverse_best_changes.yaml'])
+    opt.set_lattice_file('./FEBE_Single.def')
+    opt.set_start_file('PreFEBE')
+    opt.load_best('./nelder_mead/iteration_201/changes.yaml')
+    opt.Example()
+    # opt.Simplex()
