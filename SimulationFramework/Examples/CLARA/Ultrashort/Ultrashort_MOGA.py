80caf449d1d90086285c58edd6fe4ce244519914
James Jones <james.jones@stfc.ac.uk>
2019-09-18 17:38:43 +0100

    Update Ultrashort_MOGA.py

--- SimulationFramework/Examples/CLARA/Ultrashort/Ultrashort_MOGA.py
+++ SimulationFramework/Examples/CLARA/Ultrashort/Ultrashort_MOGA.py
@@ -8,6 +8,7 @@ import traceback
 import SimulationFramework.Modules.id_number as idn
 import SimulationFramework.Modules.id_number_server as idnserver
 from copy import copy
+import deap, deap.base, deap.creator
 
 populationSize = 10
 nChildren = 2*populationSize
@@ -118,21 +119,23 @@ moga = UltrashortMOGA()
 moga.set_lattice_file('Lattices/clara400_v12_v3.def')
 moga.start_lattice = 'generator'
 
+if os.name == 'nt':
+    nProc = 8 #change this, if more than 1 doesn't show useful errors
+else:
+    nProc = 11
+moga.create_toolbox()
+moga.create_fitness_function(optfunc)
+moga.create_weights_function(weights=(-1.0, 1.0, )) #minus is minimise
+moga.create_uniform_mating_function(probability=0.3)
+# gun phase, gun sol, l01 sol1, l01 sol2, l01 amp, l01 phase, l02 amp, l02 phase, l03 amp, l03 phase, l4h amp, l4h phase, l04 amp, l04 phase
+moga.create_gaussian_mutation_function(probability=0.3, mu=0, sigma=[1, 0.01, 0.01, 0.01, 1e6, 1, 1e6, 1, 1e6, 1, 1e6, 1, 1e6, 1])
+# moga.add_bounds(MIN, MAX)
+moga.create_NSGA2_selection_function()
+
 if __name__ == "__main__":
-    if os.name == 'nt':
-        nProc = 1 #change this, if more than 1 doesn't show useful errors
-    else:
-        nProc = 11
-    moga.create_toolbox()
-    moga.create_fitness_function(optfunc)
-    moga.create_weights_function(weights=(-1.0, 1.0, )) #minus is minimise
-    moga.create_uniform_mating_function(probability=0.3)
-    # gun phase, gun sol, l01 sol1, l01 sol2, l01 amp, l01 phase, l02 amp, l02 phase, l03 amp, l03 phase, l4h amp, l4h phase, l04 amp, l04 phase
-    moga.create_gaussian_mutation_function(probability=0.3, mu=0, sigma=[1, 0.01, 0.01, 0.01, 1e6, 1, 1e6, 1, 1e6, 1, 1e6, 1, 1e6, 1]) 
-    # moga.add_bounds(MIN, MAX)
-    moga.create_NSGA2_selection_function()
     best = moga.load_best('./GA_best_changes.yaml')
     moga.initialise_population(best, populationSize, sigma=[5, 0.05, 0.05, 0.05, 5e6, 5, 5e6, 5, 5e6, 5, 5e6, 5, 5e6, 5])
+
     if nProc > 1:
         pool = multiprocessing.Pool(processes=nProc)
         moga.toolbox.register("map", pool.map)
