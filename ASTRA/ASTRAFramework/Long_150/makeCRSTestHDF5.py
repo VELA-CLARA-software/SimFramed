0a79d2bd7254a38ed7517894e41ba336f28d759d
James Jones <james.jones@stfc.ac.uk>
2020-07-23 16:47:40 +0100

    Deleted folder that are no longer needed

--- ASTRA/ASTRAFramework/Long_150/makeCRSTestHDF5.py
+++
@@ -1,5 +0,0 @@
-from ASTRAInjector import *
-
-astra = ASTRAInjector('CSRTest_Base', overwrite=False)
-astra.loadSettings('short_240_12b3.settings')
-astra.createHDF5Summary(reference='CSRTest_Base')
