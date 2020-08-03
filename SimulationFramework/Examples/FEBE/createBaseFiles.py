2a9a4aab83f0634344cf52e732d378c6713b31a5
James Jones <james.jones@stfc.ac.uk>
2020-08-03 17:43:09 +0100

    FEBE before L01 added
    Former-commit-id: 48bf6c1d75c69509c3416bb34839fc9899f8bb9d

--- SimulationFramework/Examples/FEBE/createBaseFiles.py
+++ SimulationFramework/Examples/FEBE/createBaseFiles.py
@@ -1,21 +1,19 @@
 import os, sys
-sys.path.append(os.path.abspath(__file__+'/../../../'))
+sys.path.append(os.path.abspath(__file__+'/../../../../'))
 from SimulationFramework.Framework import *
 
 ''' Run the injector part once if only optimising post-injector parameters'''
 def create_base_files(scaling, changes=None):
     framework = Framework('basefiles_'+str(scaling), overwrite=True)
-    framework.loadSettings('FEBE.def')
+    framework.loadSettings('FEBE_Single_L01.def')
     if not os.name == 'nt':
-        framework.defineASTRACommand(['mpiexec','-np',str(4*scaling),'/opt/ASTRA/astra_MPICH2.sh'])
-        framework.defineGeneratorCommand(['/opt/ASTRA/generator.sh'])
-        framework.defineCSRTrackCommand(['/opt/OpenMPI-1.4.3/bin/mpiexec','-n',str(4*scaling),'/opt/CSRTrack/csrtrack_openmpi.sh'])
+        framework.defineASTRACommand(scaling=scaling)
     framework.generator.number_of_particles = 2**(3*scaling)
     if not changes is None:
         framework.load_changes_file(changes)
     framework.track()
 
 ## Modify as appropriate! ##
-for i in [5]:
+for i in [6]:
     create_base_files(i, './nelder_mead_best_changes.yaml')
 exit()
